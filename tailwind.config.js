5dff5827b2684a7ffe7c37082ca37b873671609f
Kevin Aton <kevinaton@Kevins-MacBook-Pro.local>
2023-03-03 14:56:35 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/style.css",
+    "./public/index.html",
+    "./public/style.css"          
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        "kev-darkslate": "#1c2635",
+        "kev-darkslate2": "#161D2F"
+      },
+      backgroundImage: {
+        'hero': "url('../public/img/bkg.png')",
+      }
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px"
+      }
+    }
+  },
+  plugins: [],
+}
